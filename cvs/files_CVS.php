e25f1c86e4c8c1146da0afbe514c7a1562cac9a4
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-02-03 17:45:50 +0000

    Unificado as vari�veis de configura��o do CVS no config.php.inc. Limpeza geral (ainda).

--- cvs/files_CVS.php
+++ cvs/files_CVS.php
@@ -3,19 +3,16 @@
 require ("function_cvs.inc");
 require ("../function.inc");
 
-	$dbh = db_connect();
+$dbh = db_connect();
 	
-	# seleciona base de dados
-	mysql_select_db($dbname,$dbh);
+# seleciona base de dados
+mysql_select_db($dbname,$dbh);
       
 $sql = mysql_query("SELECT ident FROM paginas order by ident",$dbh);	
 
 while ($tupla = mysql_fetch_array($sql)) {
-
 	$ident = $tupla[ident];		
-
 	add_cvs($ident,"html/");
-
 }
 
 ?>
