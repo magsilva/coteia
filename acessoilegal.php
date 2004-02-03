4451de1cb0e9655a4fa927127c625eda07b7b0ed
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-02-03 17:49:19 +0000

    *** empty log message ***

--- acessoilegal.php
+++ acessoilegal.php
@@ -1,16 +1,20 @@
-<html>
-<head>
-<title>Acesso Ilegal</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-<body bgcolor="#FFFFFF">
-<h1>Acesso negado. Entre por <a href="index.php">aqui</a>.</h1>
-<hr size="3" width="800" align="left">
-Em caso de d&uacute;vidas, entre em contato com o <a href="mailto:
-<?
-include("function.inc");
-echo $ADMIN;
+<?php
+include_once( "config.php.inc" );
 ?>
-">administrador</a>.
+
+<html>
+
+<head>
+	<title>Acesso Ilegal</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+
+<h1>Acesso negado. Entre por <a href="index.php">aqui</a>.</h1>
+
+<hr size="3" width="70%" align="left">
+Em caso de dúvidas, entre em contato com o <a href="mailto:<?echo $ADMIN;?> ">administrador</a>.
+
 </body>
 </html>
