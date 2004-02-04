83034be6934b91c5ceaf45f9c3688f2c03153456
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-02-04 04:39:37 +0000

    Changed the create UI, now using divs.

--- create.php
+++ create.php
@@ -58,111 +58,102 @@ if ($salva) {
         	$conteudo = table_pre($conteudo,"table");
 	}
 
-		if (stristr($conteudo,"<pre>")) {
-                        $conteudo = table_pre($conteudo,"pre");  
-                }
+	if (stristr($conteudo,"<pre>")) {
+		$conteudo = table_pre($conteudo,"pre");  
+        }
 
-		if (stristr($conteudo,"</ul>")) {
-                        $conteudo = table_pre($conteudo,"ul");  
-                }
+	if (stristr($conteudo,"</ul>")) {
+        	$conteudo = table_pre($conteudo,"ul");  
+        }
 
-		if (stristr($conteudo,"</ol>")) {
-                        $conteudo = table_pre($conteudo,"ol");  
-                }
+	if (stristr($conteudo,"</ol>")) {
+               $conteudo = table_pre($conteudo,"ol");  
+        }
 
-		if (stristr($conteudo,"<lnk>")) {
-                        $conteudo = link_interno($ident,$conteudo,$dbh);
-						}
-		
-		if (stristr($ident,".")) {
-			//links to this page
-			$i = 1;
-			$query_swiki = mysql_query("select ident,titulo from paginas where (((ident like '$id_swiki.%')  or (ident='$id_swiki')) and (conteudo like '%<lnk>$indexador</lnk>%'))",$dbh);
-			while ($tupla = mysql_fetch_array($query_swiki)) {
-				$linksto_id[$i] = $tupla[ident];
-		        	$linksto_titulo[$i] = $tupla[titulo];
-		        	$i++;
-			}
-   		} else {
-                	$linksto_id[1] = "0";
-                	$linksto_titulo[1] = "Lista de Swikis";
+	if (stristr($conteudo,"<lnk>")) {
+               $conteudo = link_interno($ident,$conteudo,$dbh);
+	}
+
+	if (stristr($ident,".")) {
+		//links to this page
+		$i = 1;
+		$query_swiki = mysql_query("select ident,titulo from paginas where (((ident like '$id_swiki.%')  or (ident='$id_swiki')) and (conteudo like '%<lnk>$indexador</lnk>%'))",$dbh);
+		while ($tupla = mysql_fetch_array($query_swiki)) {
+			$linksto_id[$i] = $tupla[ident];
+	        	$linksto_titulo[$i] = $tupla[titulo];
+	        	$i++;
 		}
+	} else {
+               	$linksto_id[1] = "0";
+               	$linksto_titulo[1] = "Lista de Swikis";
+	}
 
-		//verifica travamento da pagina
+	//verifica travamento da pagina
         if ($lock == locked) {
-			$flag_lock = 1;
-        } else $flag_lock = 0;
-		
-		$cp_java = $PATH_JAVA;
-		$cp_xt = $PATH_XT;
-		$path_xml = $PATH_XML;
-		$arq_xsl = $PATH_XSL;
-		$path_html = $PATH_XHTML;
-		$dtd = "<!DOCTYPE coteia SYSTEM 'coteia.dtd'>";
-		$node = "page";
-		$id = "id";
-		$lock_xml = "<lock>$flag_lock</lock>";
-		$others = "<sw_id>$id_swiki</sw_id>";
-		$kwd[1] = "kwd1";
-		$kwd[2] = "kwd2";
-		$kwd[3] = "kwd3";
-		$aut = "aut";
-		$tit = "tit";
-		$body = "bdy";
+		$flag_lock = 1;
+        } else {
+		$flag_lock = 0;
+	}
+	
+	$cp_java = $PATH_JAVA;
+	$cp_xt = $PATH_XT;
+	$path_xml = $PATH_XML;
+	$arq_xsl = $PATH_XSL;
+	$path_html = $PATH_XHTML;
+	$dtd = "<!DOCTYPE coteia SYSTEM 'coteia.dtd'>";
+	$node = "page";
+	$id = "id";
+	$lock_xml = "<lock>$flag_lock</lock>";
+	$others = "<sw_id>$id_swiki</sw_id>";
+	$kwd[1] = "kwd1";
+	$kwd[2] = "kwd2";
+	$kwd[3] = "kwd3";
+	$aut = "aut";
+	$tit = "tit";
+	$body = "bdy";
 
-		$query_extra = mysql_query("select id_ann,id_chat,id_eclass from swiki where id='$id_swiki'");
-        	$result = mysql_fetch_array($query_extra); 
-        	$annotation = "<ann_folder>$result[id_ann]</ann_folder>";
-        	$chat = "<chat_folder>$result[id_chat]</chat_folder>";
-		$eclass = "<id_eclass>$result[id_eclass]</id_eclass>";
- 
-if (xml_xsl($ident,$conteudo,$titulo,$autor,$keyword,$arq_xsl,$cp_xt,$cp_java,$path_html,$path_xml,$dtd,$node,$id,$lock_xml,$annotation,$chat,$eclass,$others,$linksto_id,$linksto_titulo,$kwd,$aut,$tit,$body)==TRUE) {
+	$query_extra = mysql_query("select id_ann,id_chat,id_eclass from swiki where id='$id_swiki'");
+       	$result = mysql_fetch_array($query_extra); 
+       	$annotation = "<ann_folder>$result[id_ann]</ann_folder>";
+       	$chat = "<chat_folder>$result[id_chat]</chat_folder>";
+	$eclass = "<id_eclass>$result[id_eclass]</id_eclass>";
 
-         	//adiciona arquivo no CVS
+	if (xml_xsl($ident,$conteudo,$titulo,$autor,$keyword,$arq_xsl,$cp_xt,$cp_java,$path_html,$path_xml,$dtd,$node,$id,$lock_xml,$annotation,$chat,$eclass,$others,$linksto_id,$linksto_titulo,$kwd,$aut,$tit,$body)==TRUE) {
+		//adiciona arquivo no CVS
                 add_cvs($ident,"html/");
 
 		$nro_ip= getenv("REMOTE_ADDR"); 
 		$d = getdate();
-	    	$data=$d["year"]."-".$d["mon"]."-".$d["mday"]." ".$d["hours"].":".$d["minutes"].":".$d["seconds"];
+    		$data=$d["year"]."-".$d["mon"]."-".$d["mday"]." ".$d["hours"].":".$d["minutes"].":".$d["seconds"];
 		
 		if ($flag_lock == 1) {
-		$query = "insert into paginas (ident,indexador,titulo,conteudo,ip, data_criacao,data_ultversao,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo','$conteudo_puro','$nro_ip','$data','$data', '$passwd','$keyword[1]','$keyword[2]','$keyword[3]','$autor')" or die  ("Falha ao inserir no Banco de Dados");
-		$sql = mysql_query("$query",$dbh);
+			$query = "insert into paginas (ident,indexador,titulo,conteudo,ip, data_criacao,data_ultversao,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo','$conteudo_puro','$nro_ip','$data','$data', '$passwd','$keyword[1]','$keyword[2]','$keyword[3]','$autor')" or die  ("Falha ao inserir no Banco de Dados");
+			$sql = mysql_query("$query",$dbh);
  
-		//$query_back = "insert into backup (ident,indexador,titulo,conteudo,ip, data,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo','$conteudo_puro','$nro_ip', '$data','$passwd','$keyword[1]','$keyword[2]','$keyword[3]','$autor')";
-		//$sql_back = mysql_query("$query_back",$dbh);
+		} else {
+			$query = "insert into paginas (ident,indexador,titulo,conteudo,ip, data_criacao, data_ultversao ,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo', '$conteudo_puro', '$nro_ip','$data','$data',NULL,'$keyword[1]','$keyword[2]','$keyword[3]','$autor')" or die ("Falha ao inserir no Banco de Dados");
+			$sql = mysql_query("$query",$dbh);
 		}
-		else {
-		$query = "insert into paginas (ident,indexador,titulo,conteudo,ip, data_criacao, data_ultversao ,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo', '$conteudo_puro', '$nro_ip','$data','$data',NULL,'$keyword[1]','$keyword[2]','$keyword[3]','$autor')" or die ("Falha ao inserir no Banco de Dados");
-		$sql = mysql_query("$query",$dbh);
- 
-		//$query_back = "insert into backup (ident,indexador,titulo,conteudo,ip, data,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo','$conteudo_puro','$nro_ip','$data', NULL,'$keyword[1]','$keyword[2]','$keyword[3]','$autor')";
-		//$sql_back = mysql_query("$query_back",$dbh);
-		}
-
+		
 		$query = "insert into gets (id_pag,id_sw,data) values ('$ident','$id_swiki','$data')" or die ("Falha ao inserir no Banco de Dados");
 		$sql = mysql_query("$query",$dbh);
-		
-		}
-		else	{
-				//nao criou arquivo fisico >> erro 
-				$st = 2;
-				include("erro.php" );
-				exit();
-		}
+	} else {
+		//nao criou arquivo fisico >> erro 
+		$st = 2;
+		include("erro.php" );
+		exit();
+	}
 
-        if (stristr($ident,".")) {
-                $pos_lstdot = strrpos($ident,".");   
-                $ident_pai = substr($ident,0,$pos_lstdot);
+	if (stristr($ident,".")) {
+		$pos_lstdot = strrpos($ident,".");
+		$ident_pai = substr($ident,0,$pos_lstdot);
                  
-                include("atualiza.php");
+	        include("atualiza.php");
 		//atualiza pagina pai
-                update_cvs($ident_pai,"html/");
+       	        update_cvs($ident_pai,"html/");
+       }
 
-        }
-
-		header("Location:mostra.php?ident=$ident");
-	            
+	header("Location:mostra.php?ident=$ident");
 } else {
 ?>
 <html>
@@ -187,6 +178,41 @@ if (xml_xsl($ident,$conteudo,$titulo,$autor,$keyword,$arq_xsl,$cp_xt,$cp_java,$p
 		return true;
 	}
 	</script>
+	<style type="text/css">
+		div.content {
+			text-align: left;
+			border-width:0px;
+			margin: 5px;
+			border:solid;
+			margin-top: 10ex;
+			margin-left: 2ex;
+			margin-right: 2ex;
+			float:none;
+			position:fixed;
+		}
+		div.metadata {
+			position: relative;
+			text-align: left;
+			border-width:0px;
+			margin: 5px;
+			border:solid;
+			float: left;
+			margin-left: 2ex;
+			margin-right: 2ex;
+			top: -2em;
+		}
+		div.lock {
+			position: relative;
+			text-align: right;
+			border-width:0px;
+			margin: 5px;
+			border:solid;
+			float: right;
+			margin-left: 2ex;
+			margin-right: 2ex;
+			top: -2em;
+		}
+	</style>
 </head>
 
 <body>
@@ -206,45 +232,57 @@ if (xml_xsl($ident,$conteudo,$titulo,$autor,$keyword,$arq_xsl,$cp_xt,$cp_java,$p
 <br />
 
 <form method="post" action="create.php" name="create" onSubmit="return validar();">
+<div class="lock">
+	Lock
+	<br /><input type="checkbox" name="lock" value="locked" />
 
-<table width="100%" border="0">
-  <tr bgcolor="FFFFCC">
-      <td><INPUT TYPE="checkbox" name="lock" value="locked"> - Lock<br><br></td>
-  </tr>
-  <tr bgcolor="FFFFCC">
-      <td><INPUT TYPE="password" size="10" name="passwd" onBlur="window.document.create.lock.checked=true;return false;"> - Password<br><br></td>
-  </tr>
-  <tr bgcolor="FFFFCC">
-      <td><INPUT TYPE="password" size="10" name="repasswd"> - Reenter Password</td>
-  </tr>
-  <tr><td>
-                <font color="#000055" size="5" align="center">T&iacute;tulo
-                <INPUT TYPE="text" NAME="titulo" VALUE="<?echo $index?>" SIZE="45"></font><br><br>   
-        </td></tr>
- <tr><td>
-		<font color="#000055" size="5" align="center">Autor
-		<INPUT TYPE="text" NAME="cria_autor" SIZE="45"></font><br><br>
- </td></tr>
- <tr><td>
-		<font color="#000055" size="5" align="center">Palavras Chave
-		</td></tr><tr><td>
-		<INPUT TYPE="text" NAME="key1" SIZE="15">
-		<INPUT TYPE="text" NAME="key2" SIZE="15">
-		<INPUT TYPE="text" NAME="key3" SIZE="15">
-		</font><br><br>
-  </td></tr>
-  <tr>
- <td><font color="#000055" size="6" align="center">Conteúdo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<INPUT TYPE="reset" value="Limpa" >&nbsp;&nbsp;&nbsp;&nbsp;<INPUT TYPE="submit" name="salva" value="Salva" ></td>
-	</tr>
-  <tr><td> 	
-	<TEXTAREA NAME="cria_conteudo" wrap=virtual ROWS="20" COLS="100">Edite o conte&uacute;do aqui...</TEXTAREA></font><br><br>  
-  </td></tr>
-  </table>
-<INPUT TYPE="hidden" name="ident" value="<?echo $ident?>"  >
-<INPUT TYPE="hidden" name="indexador" value="<?echo $index?>"  >
-</FORM>
-</BODY>
-</HTML>
+	<br />Password
+	<br /><input type="password" size="10" name="passwd" onBlur="window.document.create.lock.checked=true;return false;" />
+
+	<br />Re-enter password
+	<br /><input type="password" size="10" name="repasswd" />
+</div>
+
+<div class="metadata">
+<table>
+<tr>
+	<td>
+        	Título
+		<br /><input type="text" name="titulo" value="<?echo $index?>" SIZE="45" />
+	</td>
+</tr>
+<tr>
+	<td>
+		Autor
+		<br /><input type="text" name="cria_autor" size="45" />
+	</td>
+</tr>
+<tr>
+	<td>
+		Palavras-chave:
+		<br />
+		<INPUT TYPE="text" NAME="key1" SIZE="15" />
+		<INPUT TYPE="text" NAME="key2" SIZE="15" />
+		<INPUT TYPE="text" NAME="key3" SIZE="15" />
+	</td>
+</tr>
+</table>
+</div>
+
+<div class="content">
+	<input type="reset" value="Limpa" />
+	<input type="submit" name="salva" value="Salva" />
+		<br />
+	<textarea name="cria_conteudo" wrap=virtual rows="20" cols="100" />
+</div>
+ 
+<input type="hidden" name="ident" value="<?echo $ident?>"  />
+<input type="hidden" name="indexador" value="<?echo $index?>"  />
+</form>
+
+</body>
+
+</html>
 <? 
 }
 ?>
