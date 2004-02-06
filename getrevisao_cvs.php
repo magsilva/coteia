e5db7849e0c5ba8ce713dbe92c142560399539f3
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-02-06 01:21:30 +0000

    Added a CSS style file. Fixed some wrongly replaced tags (URL_img -> URL_IMG).

--- getrevisao_cvs.php
+++ getrevisao_cvs.php
@@ -28,36 +28,36 @@ if ($ident[1] != ".") {
 ?>
 <body>
 		<a href="mostra.php?ident=<?php echo $ident; ?>">
-			<img src="<?php echo $URL_img?>/view.png" />
+			<img src="<?php echo $URL_IMG?>/view.png" />
 		</a>
-		<img src="<?php echo $URL_img?>/editbw.png" />
+		<img src="<?php echo $URL_IMG?>/editbw.png" />
 		<a href="history.php?ident=<?php echo $ident?>">
-			<img src="<?php echo $URL_img?>/history.png" />
+			<img src="<?php echo $URL_IMG?>/history.png" />
 		</a>
 		<a href="mostra.php?ident=<?php echo $id_swiki?>">
-			<img src="<?php echo $URL_img?>/indice.png" />
+			<img src="<?php echo $URL_IMG?>/indice.png" />
 		</a>
 		<a href="JavaScript:AbreMapa(<?php echo $id_swiki?>)">
-			<img src="<?php echo $URL_img?>/map.png" />
+			<img src="<?php echo $URL_IMG?>/map.png" />
 		</a>
 		<a href="changes.php?ident=<?php echo $ident?>">
-			<img src="<?php echo $URL_img?>/changes.png" />
+			<img src="<?php echo $URL_IMG?>/changes.png" />
 		</a>
 		<a href="upload.php?ident=<?php echo $ident?>">
-			<img src="<?php echo $URL_img?>/upload.png" />
+			<img src="<?php echo $URL_IMG?>/upload.png" />
 		</a>
 		<a href="search.php?ident=<?php echo $ident?>">
-			<img src="<?php echo $URL_img?>/search.png" />
+			<img src="<?php echo $URL_IMG?>/search.png" />
 		</a>
 		<a href="help.php">
-			<img src="<?php echo $URL_img?>/help.png" />
+			<img src="<?php echo $URL_IMG?>/help.png" />
 		</a>
 		<a href="JavaScript:AbreChat(<?php echo $id_swiki?>)">
-			<img src="<?php echo $URL_img?>/chat.png" />
+			<img src="<?php echo $URL_IMG?>/chat.png" />
 		</a>
-		<img src="<?php echo $URL_img?>/notebw.png" />
+		<img src="<?php echo $URL_IMG?>/notebw.png" />
 		<a href="JavaScript:Imprime()">
-			<img src="<?php echo $URL_img?>/print.png"/>
+			<img src="<?php echo $URL_IMG?>/print.png"/>
 		</a>
 <br />
 <?php 
