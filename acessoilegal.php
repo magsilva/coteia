346e69480250c43893575f0ad1032b6d8f5e868c
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-02-10 17:59:06 +0000

    Making the pages XHTML compliant.

--- acessoilegal.php
+++ acessoilegal.php
@@ -6,14 +6,14 @@ include_once( "config.php.inc" );
 
 <head>
 	<title>Acesso Ilegal</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 
 <body>
 
 <h1>Acesso negado. Entre por <a href="index.php">aqui</a>.</h1>
 
-<hr size="3" width="70%" align="left">
+<hr size="3" width="70%" />
 Em caso de dúvidas, entre em contato com o <a href="mailto:<?echo $ADMIN;?> ">administrador</a>.
 
 </body>
--- add-annotation.php
+++ add-annotation.php
@@ -1,105 +1,92 @@
 <html>
+
 <head>
-<title>Formul&aacute;rio GroupNote</title>
+	<title>Formulário GroupNote</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<script type="text/javascript" src="coteia.js"></script>
+	<link href="coteia.css" rel="stylesheet" type="text/css" />
 </head>
-<body text="#000000" vLink="#cc0000" aLink="#cccc00" link="#0000ff" bgColor="#ffffff">
+
+<body>
 
 <?php
-  //echo "iduser: $id_usuario idgroup:$id_grupo<br>";
+include_once("function.inc");  
 
-  include_once("function.inc");  
-  
-  // No caso de anotacoes internas  
-  $array_temp = explode(".", $sw_id);
-  $sw_id = $array_temp[0];
-
-  $is_login = annotationLogin($sw_id);
-
-if ($add_an) {
-   
-  if ($is_login == TRUE) {
-
-      // verifica se o grupo existe
-      if (!group_exists($form_grupo))
-          msg("Grupo não existe!");
-
-      // autentica o usuario
-      $ret = authenticate_user($form_login, $form_pass);
-      if ($ret == -1){ // login inválido
-          msg("Login inv&aacute;lido!");
-      }
-      else if ($ret == 0){ // senha inválida
-          msg("Senha inv&aacute;lida!");
-      }
-      else{
-          $id_usuario = get_user_id("$form_login");
-          $id_grupo   = get_group_id("$form_grupo");
-
-          if (!user_exists_in_group($id_usuario, $id_grupo)){
-	      msg("Usuário não pertence ao grupo!");
-          }
-      } // end else
-  } // end if ($is_login)
-
-
-  $kwd["kwd1"] = $form_kwd1;  
-  $kwd["kwd2"] = $form_kwd2;
-  $kwd["kwd3"] = $form_kwd3;
-//echo "idpasta:$id_pasta, iduser:$id_usuario, idgrupo:$id_grupo";
-
-  $ret = create_annotation ($id_pasta, $id_father, $id_usuario, $id_grupo,"text/xhtml","0","",$form_titulo,$kwd,$annotates,"",$form_texto);
-
-  if ($ret > 0)
-      include_once("anotacao.php");
-  else
-      msg("Erro na criação da anotação!");
-	
+function msg($str){
+	echo "<center><b>$str</b><br><a href=javascript:history.back()>Voltar</a></center>";
+	echo "</body>";
+	echo "</html>";
+	exit();
 }
-else {
-?>
-<h2>Anota&ccedil;&atilde;o</h2>
-<form name="form_criaAnot" method="post" action="add-annotation.php">
-<b>T&iacute;tulo:</b><br>
-<input type="text" name="form_titulo" size=30><br>
-<b>Conte&uacute;do</b><br>
-<textarea name="form_texto" cols=65 rows=10 wrap=virtual>
-Insira o conte&uacute;do aqui.
-</textarea><br>
-<br>
-<?php
-  if ($is_login == TRUE){
-     echo "<b>User:</b> <input type='text' name='form_login' size=8>&nbsp;&nbsp;";
-     echo "<b>Grupo:</b> <input type='text' name='form_grupo' size=15>&nbsp;&nbsp;";
-     echo "<b>Senha:</b> <input type='password' name='form_pass' size=8>";
-   }
-?>
-<br><br>
-<input name="add_an" type="submit" value="enviar"><br>
-<input name="canc_an" type="button" value="voltar &agrave; p&aacute;gina anterior" 
-onClick="javascript:history.back()">
-<input name="close_an" type="button" value="fechar a p&aacute;gina" 
-onClick="javascript:window.close()">
 
-<input name="id_pasta" type="hidden" value="<?echo $id_pasta?>">
-<input name="id_usuario" type="hidden" value="<?echo $id_usuario?>">
-<input name="id_grupo" type="hidden" value="<?echo $id_grupo?>">
-<input name="pasta" type="hidden" value="<?echo $pasta?>">
-<input name="annotates" type="hidden" value="<?echo $annotates?>">
-<input name="id_father" type="hidden" value="<?echo $id_father?>">
-<input name="sw_id" type="hidden" value="<?echo $sw_id?>">
+// No caso de anotacoes internas  
+$array_temp = explode( ".", $sw_id );
+$sw_id = $array_temp[ 0 ];
+$is_login = annotationLogin( $sw_id );
+
+if ( $add_an ) {
+	if ( $is_login == TRUE ) {
+		// verifica se o grupo existe
+		if ( !group_exists( $form_grupo ) ) {
+         msg( "Grupo não existe!" );
+		}
+		// autentica o usuario
+		$ret = authenticate_user( $form_login, $form_pass );
+		if ( $ret == -1 ) {
+			msg("Login inválido!");
+		} else if ( $ret == 0 ) {
+			msg( "Senha inválida!" );
+		} else {
+			$id_usuario = get_user_id( "$form_login" );
+			$id_grupo = get_group_id( "$form_grupo" );
+			if ( !user_exists_in_group( $id_usuario, $id_grupo ) ) {
+				msg( "Usuário não pertence ao grupo!" );
+			}
+		}
+	}
+	$kwd[ "kwd1" ] = $form_kwd1;
+	$kwd[ "kwd2" ] = $form_kwd2;
+	$kwd[ "kwd3" ] = $form_kwd3;
+	$ret = create_annotation( $id_pasta, $id_father, $id_usuario, $id_grupo, "text/xhtml", "0", "", $form_titulo, $kwd, $annotates, "", $form_texto );
+
+	if ( $ret > 0 ) {
+		include_once( "anotacao.php" );
+	} else {
+		msg( "Erro na criação da anotação!" );
+	}
+} else {
+?>
+<h2>Anotação</h2>
+
+<form name="form_criaAnot" method="post" action="add-annotation.php">
+	<b>Título:</b>
+	<br /><input type="text" name="form_titulo" size=30 />
+	<br /><b>Conteúdo</b>
+	<br /><textarea name="form_texto" cols=65 rows=10 wrap=virtual style="width: 100%"></textarea>
+	<br />
+<?php
+	if ( $is_login == TRUE ) {
+		echo "<br /><b>User:</b> <input type='text' name='form_login' size=8 />";
+		echo "<br /><b>Grupo:</b> <input type='text' name='form_grupo' size=15 />";
+		echo "<br /><b>Senha:</b> <input type='password' name='form_pass' size=8 />";
+	}
+?>
+	<br /><input name="add_an" type="submit" value="enviar" />
+	<br /><input name="canc_an" type="button" value="voltar para a página anterior" onClick="javascript:history.back()" />
+	<input name="close_an" type="button" value="fechar a página" onClick="javascript:window.close()" />
+
+	<input name="id_pasta" type="hidden" value="<?php echo $id_pasta?>" />
+	<input name="id_usuario" type="hidden" value="<?php echo $id_usuario?>" />
+	<input name="id_grupo" type="hidden" value="<?php echo $id_grupo?>" />
+	<input name="pasta" type="hidden" value="<?php echo $pasta?>" />
+	<input name="annotates" type="hidden" value="<?php echo $annotates?>" />
+	<input name="id_father" type="hidden" value="<?php echo $id_father?>" />
+	<input name="sw_id" type="hidden" value="<?php echo $sw_id?>" />
 </form>
 
 </body>
+
 </html>
 <?
 }
- 
-function msg($str){
-   echo "<center><b>$str</b><br><a href=javascript:history.back()>Voltar</a></center>";
-   echo "</body>";
-   echo "</html>";
-   exit();
-}
-
 ?>
-
