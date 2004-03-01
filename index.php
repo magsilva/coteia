ba640a3db6dc8da903336be6392253b6332084cd
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-03-01 14:19:18 +0000

    Fixed the array usage when getting SQL queried data (sql[desiredcolumn] != sql["desiredcolumn"], who knows what miracle php was doing to this work correctly...).

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if ($month <= '6') {
 	$semester = 2;
 }
 
-echo "<b>Semestre Atual: $semester&ordm; de $year</b><br /><ul>";
+echo "<b>Semestre Atual: $semester&ordm; de $year</b><br /><ul>\n";
 
 //semestre atual
 $sem_atual = $semester . '_' . $year;
@@ -39,30 +39,29 @@ $dbh = db_connect();
 mysql_select_db($dbname,$dbh);
 $sql = mysql_query("SELECT id,status,titulo,id_chat,admin,admin_mail,visivel FROM swiki where (semestre='$sem_atual' || semestre='T') order by titulo",$dbh);	
 while ($tupla = mysql_fetch_array($sql)) {
-	if ($tupla[visivel]=='S') {
-		if (!empty($tupla[titulo])) {
-			$final = $tupla[titulo];
+	if ($tupla["visivel"]=='S') {
+		if (!empty($tupla["titulo"])) {
+			$final = $tupla["titulo"];
 		}
-		$session_id = $tupla[id_chat];
-		$admin = $tupla[admin];
-		$admail = $tupla[admin_mail];
+		$session_id = $tupla["id_chat"];
+		$admin = $tupla["admin"];
+		$admail = $tupla["admin_mail"];
 
-		$ident = $tupla[id];
-		$status = $tupla[status];
+		$ident = $tupla["id"];
+		$status = $tupla["status"];
 		$sql_aux = mysql_query("SELECT id_sw FROM gets",$dbh);
 		
 		//$ident recebe o número de paginas ja criadas na swiki (relacionadas na tabela GETS)
 		$query_cont = "SELECT COUNT(*) as CONTADOR from gets where id_sw='$ident'";
 		$sql_cont = mysql_query("$query_cont",$dbh);
 		$tupla_cont = mysql_fetch_array($sql_cont);
-		$nro_paginas = $tupla_cont[CONTADOR];
+		$nro_paginas = $tupla_cont["CONTADOR"];
 
-		$controle=0;
-		while (($tupla_aux = mysql_fetch_array($sql_aux)) && ($controle==0)) {
-			$comp = $tupla_aux[id_sw];
-			if ($ident==$comp) {
+		$token = false;
+		while ( ($tupla_aux = mysql_fetch_array( $sql_aux ) ) && ( $token == false ) ) {
+			$comp = $tupla_aux[ "id_sw" ];
+			if ( $ident == $comp ) {
 				$token=true;
-				$controle=1;
 			}	else {
 				$token=false;
 			}
@@ -70,53 +69,52 @@ while ($tupla = mysql_fetch_array($sql)) {
 
 		if ($token==true) {
 			if ($status == '1') {
-				echo "\t<li><a href=\"#\" onClick=\"window.open('login.php?id=$ident&amp;token=1','login','top=50,left=100,menubar=no,resizable=no,width=300,height=200')\">$final</a> ($nro_paginas) página(s):  (administrador: <a href=\"mailto:$admail\">$admin</a>)</li>\n";
+				echo "\t<li><a href=\"#\" onClick=\"window.open('login.php?id=$ident&token=1','login','top=50,left=100,menubar=no,resizable=no,width=300,height=200')\">$final</a> ($nro_paginas) página(s):  (administrador: <a href=\"mailto:$admail\">$admin</a>)</li>\n";
 			} else {
 				echo "\t<li><a href=\"mostra.php?ident=$ident\" onMouseOver=\"window.status='$final'; return true\" onMouseOut=\"window.status=' '; return true\">$final</a> ($nro_paginas) página(s):  (administrador: <a href=\"mailto:$admail\">$admin</a>)</li>\n";
 			}
 		} else {
 			$final_url = rawurlencode($final);
 			if ($status == '1') {
-				echo "\t<li>$final<a href=\"#\" onClick=\"window.open('login.php?id=$ident&amp;token=0&amp;index=$final_url','login','top=50,left=100,menubar=no,resizable=no,width=300,height=200')\">[create]</a></li>\n";
+				echo "\t<li>$final<a href=\"#\" onClick=\"window.open('login.php?id=$ident&token=0&index=$final_url','login','top=50,left=100,menubar=no,resizable=no,width=300,height=200')\">[create]</a></li>\n";
 			} else {
-				echo "\t<li>$final<a href=\"create.php?ident=$ident&amp;index=$final_url\" onmouseover=\"window.status='$final'; return true\" onmouseout=\"window.status=' '; return true\">[create]</a></li>\n";
+				echo "\t<li>$final<a href=\"create.php?ident=$ident&index=$final_url\" onmouseover=\"window.status='$final'; return true\" onmouseout=\"window.status=' '; return true\">[create]</a></li>\n";
 			}
 		}
 	}
 }
 
-echo "</ul><br /><b>Outras Entradas:</b><br /><ul>";
+echo "</ul><br /><b>Outras Entradas:</b><br /><ul>\n";
 
 $sql = mysql_query("SELECT id,status,titulo,id_chat,admin,admin_mail,visivel FROM swiki where (semestre<>'$sem_atual' && semestre<>'T') order by titulo",$dbh);	
 
 while ($tupla = mysql_fetch_array($sql)) {
-	if ($tupla[visivel]=='S') {
-		if (!empty($tupla[titulo])) {
-			$final = $tupla[titulo];
+	if ($tupla["visivel"]=='S') {
+		if (!empty($tupla["titulo"])) {
+			$final = $tupla["titulo"];
 		}
 
-		$session_id = $tupla[id_chat];
-		$admin = $tupla[admin];
-		$admail = $tupla[admin_mail];
+		$session_id = $tupla["id_chat"];
+		$admin = $tupla["admin"];
+		$admail = $tupla["admin_mail"];
 
-		$ident = $tupla[id];
-		$status = $tupla[status];
+		$ident = $tupla["id"];
+		$status = $tupla["status"];
 		$sql_aux = mysql_query("SELECT id_sw FROM gets",$dbh);
 
 		//$ident recebe o número de paginas ja criadas na swiki (relacionadas na tabela GETS)
 		$query_cont = "SELECT COUNT(*) as CONTADOR from gets where id_sw='$ident'";
 		$sql_cont = mysql_query("$query_cont",$dbh);
 		$tupla_cont = mysql_fetch_array($sql_cont);
-		$nro_paginas = $tupla_cont[CONTADOR];
+		$nro_paginas = $tupla_cont["CONTADOR"];
 
-		$controle=0;
-		while (($tupla_aux = mysql_fetch_array($sql_aux)) && ($controle==0)) {
-			$comp = $tupla_aux[id_sw];
+		$token = false;
+		while (($tupla_aux = mysql_fetch_array($sql_aux)) && ($token == false)) {
+			$comp = $tupla_aux["id_sw"];
 			if ($ident==$comp) {
 				$token=true; 
-				$controle=1;
-			}	else {
-				 $token=false;
+			} else {
+				$token=false;
 			}
 		}
 
@@ -142,21 +140,21 @@ echo "</ul><br /><b>Total de Entradas:</b>";
 $query_cont = "SELECT COUNT(*) as CONTADOR from swiki where visivel='S'";  
 $sql_cont = mysql_query($query_cont,$dbh); 	
 $tupla_cont = mysql_fetch_array($sql_cont); 	 	
-$nro_swvs = $tupla_cont[CONTADOR]; 	 
+$nro_swvs = $tupla_cont["CONTADOR"];
 
 $query_cont = "SELECT COUNT(*) as CONTADOR from swiki where visivel='N'";  
 $sql_cont = mysql_query($query_cont,$dbh); 	
 $tupla_cont = mysql_fetch_array($sql_cont); 	 	
-$nro_swnvs = $tupla_cont[CONTADOR]; 	 
+$nro_swnvs = $tupla_cont["CONTADOR"];
 
 echo " $nro_swvs [+ $nro_swnvs]";
 
 echo "<br /><b>Total de Páginas:</b>";
 
 $query_cont = "SELECT COUNT(*) as CONTADOR from paginas";  
-$sql_cont = mysql_query($query_cont,$dbh); 	
-$tupla_cont = mysql_fetch_array($sql_cont); 	 	
-$nro_pgs = $tupla_cont[CONTADOR]; 	 
+$sql_cont = mysql_query($query_cont,$dbh);
+$tupla_cont = mysql_fetch_array($sql_cont);
+$nro_pgs = $tupla_cont["CONTADOR"];
 
 echo " $nro_pgs<br />";
 
