5f8113ccda659fc67ae4bad4bcc277587f945308
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-03-18 16:50:37 +0000

    Removed a dummy echo I forgot when solving bug 63.

--- create.php
+++ create.php
@@ -120,7 +120,6 @@ if ($salva) {
 		$keyword[ 3 ] = addslashes( $keyword[ 3 ] );
 		$autor = addslashes( $autor );
 		$query = "insert into paginas (ident,indexador,titulo,conteudo,ip, data_criacao,data_ultversao,pass, kwd1, kwd2, kwd3,autor) values ('$ident','$indexador','$titulo','$conteudo_puro','$nro_ip','$data','$data',$passwd,'$keyword[1]','$keyword[2]','$keyword[3]','$autor')";
-		echo $query;
 		$sql = mysql_query($query,$dbh) or die ("Falha ao inserir no Banco de Dados");
 
  		$query = "insert into gets (id_pag,id_sw,data) values ('$ident','$id_swiki','$data')" or die ("Falha ao inserir no Banco de Dados");
