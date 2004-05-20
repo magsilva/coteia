9650ac30d658b146a01fb66b890ad2f64910cb66
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-05-20 12:39:38 +0000

    Added new files (needed for CoTeia's migration). Updated some Apache's generated configuration.

---
+++ tables/converte_base.php
@@ -0,0 +1,34 @@
+<?php
+  
+include_once( "../function.inc" );
+
+// Conexão ao sgbd.
+$dbh = db_connect();
+mysql_select_db( $dbname, $dbh);
+
+// Recupera todos os dados das wikipages.
+$query = "SELECT ident,pass FROM paginas";
+$result = mysql_query( $query, $dbh );
+
+// Começa a geração do dump propriamente dito, alterando tupla por tupla e gravando em arquivo.
+while ( $tupla = mysql_fetch_array( $result ) ) {
+	$ident = $tupla[ "ident" ]; 
+	$passwd = $tupla[ "pass" ];
+
+	echo "\n$ident...";
+
+	// Criptografa a senha da wikipage.
+	$passwd = md5( $passwd );
+
+	// Prepara o comando SQL do dump da tupla.
+	$wikipage_update = "update paginas set pass='$passwd' where ident='$ident'";
+
+	// Atualiza a senha da wikipage.
+	$sql = mysql_query( $wikipage_update, $dbh ) or die ("Falha ao inserir no Banco de Dados");
+	echo "Ok";
+}
+
+// libera a memoria usada pela variavel $sql
+mysql_free_result( $result );
+
+?>
