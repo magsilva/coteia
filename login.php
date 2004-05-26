b3df3887005aaedcf93d5f4dfce0187d621c4909
marco <marco@12f8599f-3982-4fa1-8f20-902d4b8a78cc>
2004-05-26 02:20:56 +0000

    Removed "autentica.php", moving the code to inside "login.php". Added a missing "rawurlencode" into login ("index" was being sent via GET without it).

--- login.php
+++ login.php
@@ -1,3 +1,38 @@
+<?php
+/*
+* Authenticate users and set session variables.
+*
+* Copyright (c) 2001, 2002, 2003 Carlos E. Arruda Jr.
+* Modified by Marco Aurélio Graciotto Silva.
+*/
+
+if ( isset( $_REQUEST[ "login" ] ) {
+
+	include_once( "function.inc" );
+
+	if ( check_wikipage_id( $_REQUEST[ "id" ] ) == false ) {
+		show_error( 0 );
+	}
+
+	$dbh = db_connect();
+	$result = login_swiki( $_REQUEST[ "username" ], $_REQUEST[ "password" ], $_REQUEST[ "id" ], $dbh );
+
+	if ( $result ) {
+		if ( $token == "1" ) {
+			header( "Location:mostra.php?ident=$id" ); 
+			exit;
+		}
+
+		if ( $token == "0" ) {
+			header( "Location:create.php?ident=$id&index=" . rawurlencode( $index ) );
+			exit;
+		}
+	} else {
+		echo '<br /><div align="center">Área Restrita.<br /><br /><a href="javascript:history.go(-1)">Voltar</a></div>';
+	}
+} else {
+?>
+
 <html>
 
 <head>
@@ -7,16 +42,14 @@
 
 <body>
 
-<table width="100%">
-
 <h1>Login Obrigatório</h1>
 
-<form name="login" METHOD="post" action="autentica.php">
+<form name="login" method="post" action="autentica.php">
 	<br /><b>Usuário:</b>
-	<br /><input type="text" name="usuario" size="16" />
+	<br /><input type="text" name="username" size="16" />
 
 	<br /><b>Senha:</b>
-	<br /><input type="password" name="passwd" size="16" />
+	<br /><input type="password" name="password" size="16" />
 
 	<br /><input type=submit value="Login" name="login" />
 
@@ -28,3 +61,7 @@
 </body>
 
 </html>
+
+<?php
+}
+?>
